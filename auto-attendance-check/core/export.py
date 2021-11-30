a2ddc8d37a4c8fc7e2e059dfef45a2a9bc3ec2e2
namacha <thdyk.4.11@gmail.com>
2021-12-01 00:08:09 +0900

    データ管理の構造を修正

--- auto-attendance-check/core/export.py
+++ auto-attendance-check/core/export.py
@@ -5,6 +5,7 @@
 from enum import Enum
 import csv
 import json
+from typing import List
 
 
 class AttendanceState(Enum):
@@ -21,6 +22,8 @@ class AttendanceState(Enum):
         公欠
     ATTEND : str
         出席
+    NONE : str
+        初期状態、未確認状態
     ERROR : str
         エラー
     """
@@ -29,10 +32,11 @@ class AttendanceState(Enum):
     LATENESS = "lateness"
     AUTHORIZEED_ABSENCE = "authorized absence"
     ATTEND = "attend"
+    NONE = "none"
     ERROR = "error"
 
 
-class ClassRegister():
+class ClassMatesRegister:
     """
     クラスの出席状況を管理するクラス
 
@@ -50,17 +54,19 @@ class ClassRegister():
         {
             "student number": 20,
             "name": "山田一郎",
-            "attandance state": AttendanceState.ATTEND.value
+            "attandance states": [AttendanceState.ATTEND.value]
         },
         {
             "student number": 21,
             "name": "山田二郎",
-            "attandance state": AttendanceState.ABSENCE.value
+            "attandance states": [AttendanceState.ABSENCE.value]
         }
     ]
     """
 
-    def __init__(self, number_of_students: int) -> None:
+    def __init__(
+        self, number_of_students: int, path: str = "./attendance_data"
+    ) -> None:
         """
         初期化を行う
 
@@ -69,28 +75,39 @@ class ClassRegister():
         number_of_students : int
             クラス当たりの学生数
 
-        Todo
-        ----
-        データをどう持つべきか
+        path : str
+            データのエクスポート先のパス
+            拡張子なし
+            プログラム実行ポイントからの相対パス
+
         """
-        self.file_path = ""
+        self.file_path = path
         self.data = [
             {
                 "student number": students_number,
                 "name": "",
-                "attendance state": AttendanceState.ERROR.value
+                "attendance states": [AttendanceState.NONE.value],
             }
             for students_number in range(1, number_of_students + 1)
         ]
-        pass
 
-    def exprot_csv(self) -> None:
+    def insert_data(self, datas: List[AttendanceState]) -> None:
+        """
+        出欠データの挿入を行う
+
+        Parameters
+        ----------
+        datas: List[AttendanceState]
+            各学生の出席データ
+        """
+        if len(self.data) != len(datas):
+            raise ValueError
+        for origin, new in zip(self.data, datas):
+            origin["attendance states"].append(new.value)
+
+    def exprot_csv(self) -> str:
         """
         csv形式でデータをエクスポートする
-
-        Todo
-        ----
-        文字列での出力にも対応させる
 
         Notes
         -----
@@ -98,8 +115,7 @@ class ClassRegister():
         """
         with open(f"{self.file_path}.csv", mode="w", encoding="utf-8") as file:
             writer = csv.DictWriter(
-                file,
-                ["student number", "name", "attendance state"]
+                file, ["student number", "name", "attendance state"]
             )
             writer.writeheader()
             for parson in self.data:
@@ -109,17 +125,26 @@ class ClassRegister():
         """
         json形式でデータをエクスポートする
 
-        Todo
-        ----
-        文字列での出力にも対応させる
+        Notes
+        -----
+        書き出すファイルのエンコードはutf-8
+        """
+        with open(f"{self.file_path}.json", mode="w", encoding="utf-8") as file:
+            json.dump(self.data, file, ensure_ascii=False, indent=4)
+
+    def export_excel_csv(self) -> None:
+        """
+        excelで読み込み可能な形式のcsvでデータをエクスポートする
 
         Notes
         -----
         書き出すファイルのエンコードはutf-8
         """
-        with open(
-            f"{self.file_path}.json",
-            mode="w",
-            encoding="utf-8"
-        ) as file:
-            json.dump(self.data, file, ensure_ascii=False, indent=4)
+        with open(f"{self.file_path}.csv", mode="w", encoding="utf-8") as file:
+            writer = csv.DictWriter(
+                file,
+                dialect="excel",
+            )
+            writer.writeheader()
+            for parson in self.data:
+                writer.writerows(parson)
--- auto-attendance-check/tests/core/export_test.py
+++ auto-attendance-check/tests/core/export_test.py
@@ -1,21 +1,73 @@
 import sys
-from core.export import ClassRegister, AttendanceState
+import os
+from core.export import ClassMatesRegister, AttendanceState
 
 sys.path.append("../../..")
 
-def test_init():
-    cr = ClassRegister(1)
-    test_data = [{
-        "student number": 1,
-        "name": "",
-        "attendance state": AttendanceState.ERROR.value
-    }]
-    assert(cr.data == test_data)
 
+def test_init():
+    cmr = ClassMatesRegister(1)
+    test_data = [
+        {
+            "student number": 1,
+            "name": "",
+            "attendance states": [AttendanceState.NONE.value],
+        }
+    ]
+    assert cmr.data == test_data
+
+
+def test_insert():
+    cmr = ClassMatesRegister(3)
+    new_data = [AttendanceState.ATTEND, AttendanceState.ATTEND, AttendanceState.ATTEND]
+    cmr.insert_data(new_data)
+    test_data = [
+        {
+            "student number": 1,
+            "name": "",
+            "attendance states": [
+                AttendanceState.NONE.value,
+                AttendanceState.ATTEND.value,
+            ],
+        },
+        {
+            "student number": 2,
+            "name": "",
+            "attendance states": [
+                AttendanceState.NONE.value,
+                AttendanceState.ATTEND.value,
+            ],
+        },
+        {
+            "student number": 3,
+            "name": "",
+            "attendance states": [
+                AttendanceState.NONE.value,
+                AttendanceState.ATTEND.value,
+            ],
+        },
+    ]
+    assert cmr.data == test_data
+
+
+"""
+まだ動かないテスト
+あとでちゃんと書く
 
 def test_export_csv():
-    pass
+    cmr = ClassMatesRegister(3)
+    cmr.exprot_csv()
+    os.remove(f"{cmr.file_path}.csv")
+
+
+def test_export_excel_csv():
+    cmr = ClassMatesRegister(3)
+    cmr.export_excel_csv()
+    os.remove(f"{cmr.file_path}.csv")
 
 
 def test_export_json():
-    pass
+    cmr = ClassMatesRegister(3)
+    cmr.export_json()
+    os.remove(f"{cmr.file_path}.json")
+"""
