48717f4758631d37e6ca953eff61d432508d9865
unknown <Vermouth77rseidl@gmail.com>
2021-12-10 19:30:12 +0900

    fixed error

--- auto-attendance-check/tests/GUI/ssh_test.py
+++ auto-attendance-check/tests/GUI/ssh_test.py
@@ -1,11 +1,11 @@
 # test ssh.py
 
-import ssh
+from core.ssh import ConnectSSH
 
 if __name__ == "__main__":
     ssh.ConnectSSH(
-        IP_ADDRESS='192.168.2.103',
-        USER_NAME='pi',
-        PASSWORD='Pottunchin8ma',
+        IP_ADDRESS='ip アドレス',
+        USER_NAME='ユーザーネーム',
+        PASSWORD='パスワード',
         CMD='uname -a'
 )
