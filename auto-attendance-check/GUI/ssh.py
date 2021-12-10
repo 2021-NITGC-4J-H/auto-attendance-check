40295c5058047d7987ceee2410dd04f23658443c
unknown <Vermouth77rseidl@gmail.com>
2021-12-10 18:52:41 +0900

    made connecting ssh file

---
+++ auto-attendance-check/GUI/ssh.py
@@ -0,0 +1,39 @@
+# connecting ssh
+
+import paramiko
+
+def ConnectSSH(IP_ADDRESS, USER_NAME, PASSWORD, CMD):
+    """
+    指定機器へSSH接続する
+
+    Parameters
+    ----------
+    IP_ADDRESS : str
+        接続機器のipアドレス
+
+    USER_NAME : str
+        接続機器のusername
+    
+    PASSWORD : str
+        接続機器のパスワード
+    
+    CMD : str list
+        実行命令をまとめたリスト
+    """
+
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(
+        IP_ADDRESS,
+        username=USER_NAME,
+        password=PASSWORD
+    )
+
+    for cmd in CMD:
+        stdin, stdout, stderr = client.exec_command(cmd)
+        for line in stdout:
+            print(line)
+
+    client.close()
+    del client, stdin, stdout, stderr
+
